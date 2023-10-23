c1522e5e4d49dfbcfbf807470a384bd286e7926c
zhuhuidiwiwi@gmail.com <zhuhuidiwiwi@gmail.com>
2023-10-23 21:40:04 +0000

    reorg, refactor model with payment info, update doc and test

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -9,7 +9,7 @@ orders as (
 
     select
         *
-    from {{ ref("stg_orders")}}
+    from {{ ref("fct_orders")}}
 
 ),
 
@@ -20,13 +20,15 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value,
+        
     from orders
-
     group by 1
 
 ),
+
+
 
 final as (
 
@@ -36,12 +38,12 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.lifetime_value, 0) as lifetime_value
     from customers
-
     left join customer_orders using (customer_id)
-
 )
 
-select * from final
+select 
+*
+from final
