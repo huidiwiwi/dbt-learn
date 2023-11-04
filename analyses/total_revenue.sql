90451b10549055dd9769e6b2a4d04748b525e667
Huidi Zhu <zhuhuidiwiwi@gmail.com>
2023-11-04 14:49:36 +0000

    add seeds and analyses

---
+++ analyses/total_revenue.sql
@@ -0,0 +1,11 @@
+with payments as(
+    select * from {{ ref("stg_payments")}}
+),
+
+aggregated as (
+    select sum(amount) as total_revenue
+    from payments
+    where payments.status = "success"
+)
+
+select * from aggregated
--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -3,6 +3,8 @@ with
 
     orders as (select * from {{ ref("fct_orders") }}),
 
+    employees as (select * from {{ ref("employees") }}),
+
     customer_orders as (
 
         select
@@ -24,12 +26,14 @@ with
             customers.customer_id,
             customers.first_name,
             customers.last_name,
+            employees.employee_id is not null as is_employee,
             customer_orders.first_order_date,
             customer_orders.most_recent_order_date,
             coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
             coalesce(customer_orders.lifetime_value, 0) as lifetime_value
         from customers
         left join customer_orders using (customer_id)
+        left join employees using (customer_id)
     )
 
 select *
