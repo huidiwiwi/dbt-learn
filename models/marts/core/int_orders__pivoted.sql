a6be2309238913230b34a572d4ab211a49284589
Huidi Zhu <zhuhuidiwiwi@gmail.com>
2023-11-05 15:46:00 +0000

    use dbt.utils to get col value list

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -1,19 +1,8 @@
 with
     payments as (select * from {{ ref("stg_payments") }}),
 
-    {%- set methods_query -%}
-        select distinct payment_method
-        from {{ ref("stg_payments") }}
-    {%- endset -%}
+    {%- set methods = dbt_utils.get_column_values(table=ref('stg_payments'), column='payment_method') -%}
     
-    {%- set results = run_query(methods_query) -%}
-
-    {%- if execute -%}
-    {%- set methods = results.columns[0].values() -%}
-    {%- else -%}
-    {%- set methods = [] -%}
-    {%- endif -%}
-
     orders_pivoted as (
         select
             order_id,
