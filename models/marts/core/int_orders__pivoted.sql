063f3cf2d789f124e8342025d5dd75bfca2032d9
Huidi Zhu <zhuhuidiwiwi@gmail.com>
2023-11-05 15:55:57 +0000

    use dbt.utils to generate pivot table

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -2,18 +2,11 @@ with
     payments as (select * from {{ ref("stg_payments") }}),
 
     {%- set methods = dbt_utils.get_column_values(table=ref('stg_payments'), column='payment_method') -%}
-    
+
     orders_pivoted as (
         select
             order_id,
-            {% for method in methods -%}
-                sum(
-                    case when payment_method = "{{ method }}" then amount else 0 end
-                ) as {{ method }}_payments
-                {%- if not loop.last -%}
-                ,
-                {%- endif %}
-            {% endfor %}
+            {{ dbt_utils.pivot(column="payment_method", agg='sum', values=methods, then_value="amount", suffix='_payments') }}
         from payments
         group by 1
     )
